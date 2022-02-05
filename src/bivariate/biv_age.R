10967923ddb8386ac9a42779629af4b78e19b960
hicks017 <74994840+hicks017@users.noreply.github.com>
2022-02-04 17:17:59 -0800

    Update biv_age.R
    Tested for an interaction of age on the cholesterol~work_hours association

--- src/bivariate/biv_age.R
+++ src/bivariate/biv_age.R
@@ -1,5 +1,5 @@
 # Setup
-pacman::p_load('knitr', 'broom', 'survey', 'tidyverse')
+pacman::p_load('knitr', 'broom', 'survey', 'tidyverse', 'gridExtra')
 df_combined <- readRDS('output/data/combined.Rds')
 attach(df_combined)
 chol_label <- 'total cholesterol (mg/dL)'
@@ -32,3 +32,43 @@ c_age_wtd <- cov2cor(as.matrix(v_age_wtd))[1, 2] %>% format(digits = 3)
 ## ---- age_output ----
 plot_age_wtd
 model_age_tbl_wtd
+
+## ---- age_interaction ----
+# No interaction observed. Results plotted below.
+df_combined$age_group <- case_when(age <30 ~ 1, # 18-29
+                                   age >= 30 & age < 40 ~ 2, # 30-39
+                                   age >= 40 & age < 50 ~ 3, # 40-49
+                                   age >= 50 & age < 60 ~ 4, # 50-59
+                                   age >= 60 & age < 70 ~ 5, # 60-69
+                                   age >= 70 ~ 6) # 70+
+plot1 <- ggplot(subset(df_combined, age_group == 1),
+                aes(wrk_hrs, bld_tc, weight = weight_mec)) +
+  geom_point(alpha = 0.4) +
+  geom_smooth(formula = y ~ x, method = 'lm') +
+  labs(x = 'work hours', y = chol_label, title = 'Age 18-29')
+plot2 <- ggplot(subset(df_combined, age_group == 2),
+                aes(wrk_hrs, bld_tc, weight = weight_mec)) +
+  geom_point(alpha = 0.4) +
+  geom_smooth(formula = y ~ x, method = 'lm') +
+  labs(x = 'work hours', y = chol_label, title = 'Age 30-39')
+plot3 <- ggplot(subset(df_combined, age_group == 3),
+                aes(wrk_hrs, bld_tc, weight = weight_mec)) +
+  geom_point(alpha = 0.4) +
+  geom_smooth(formula = y ~ x, method = 'lm') +
+  labs(x = 'work hours', y = chol_label, title = 'Age 40-49')
+plot4 <- ggplot(subset(df_combined, age_group == 4),
+                aes(wrk_hrs, bld_tc, weight = weight_mec)) +
+  geom_point(alpha = 0.4) +
+  geom_smooth(formula = y ~ x, method = 'lm') +
+  labs(x = 'work hours', y = chol_label, title = 'Age 50-59')
+plot5 <- ggplot(subset(df_combined, age_group == 5),
+                aes(wrk_hrs, bld_tc, weight = weight_mec)) +
+  geom_point(alpha = 0.4) +
+  geom_smooth(formula = y ~ x, method = 'lm') +
+  labs(x = 'work hours', y = chol_label, title = 'Age 60-69')
+plot6 <- ggplot(subset(df_combined, age_group == 6),
+                aes(wrk_hrs, bld_tc, weight = weight_mec)) +
+  geom_point(alpha = 0.4) +
+  geom_smooth(formula = y ~ x, method = 'lm') +
+  labs(x = 'work hours', y = chol_label, title = 'Age 70+')
+grid.arrange(plot1, plot2, plot3, plot4, plot5, plot6)
